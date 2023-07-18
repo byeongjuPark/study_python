3b87546ac61fb7fa329cc3e1ee7377e9bd8e0aaf
byeongjuPark <bottlepark28@gmail.com>
2023-07-18 15:51:46 +0900

    0718 학습

--- practice/loop/str3.py
+++ practice/loop/str3.py
@@ -16,3 +16,26 @@ for x in a:
         print(x, end=' ')
 print()
 
+# x를 tuple로
+# 튜플은 값 변경이 불가능
+for x in enumerate(a):
+    print(x)
+    print(x[0], end= ', ')
+    print(x[1])
+print()
+
+for index, value in enumerate(a):
+    print(index, value) 
+print()
+
+# all() 모두가 다 참이여야 true 반환
+if all(60>x for x in a):
+    print("YES")
+else:
+    print("NO")
+
+# any 한번이라도 참이면 true
+if any(60>x for x in a):
+    print("YES")
+else:
+    print("NO")
